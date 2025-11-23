848efd0691c10678d1ebe2035eba4e2347f8bd42
Camilo <klibrebeat@gmail.com>
2025-11-23 16:34:34 -0300

    Se agrego manejo de errores en funciones de lectura de disco y revision de usuarios conectados

--- analisis_server.py
+++ analisis_server.py
@@ -2,30 +2,38 @@ import os
 import shutil
 import datetime
 
-# 1. Función para chequear espacio en disco
+# Verificamos el disco raíz porque si se llena al 100%, el servidor Linux deja de responder.
+
 def check_disk_usage(path="/"):
-    total, used, free = shutil.disk_usage(path)
-    percent_used = (used / total) * 100
+
+    try: 
+        total, used, free = shutil.disk_usage(path)
+        percent_used = (used / total) * 100
     
-    print(f"--- Auditoría de Disco: {path} ---")
-    print(f"Total: {total // (2**30)} GB")
-    print(f"Usado: {used // (2**30)} GB")
-    print(f"Libre: {free // (2**30)} GB")
-    print(f"Porcentaje de uso: {percent_used:.2f}%")
-    
-    if percent_used > 80:
-        print("ALERTA: ¡Espacio crítico! Revisa el servidor.")
-    else:
-        print("Estado: Saludable.")
+        print(f"--- Auditoría de Disco: {path} ---")
+        print(f"Total: {total // (2**30)} GB")
+        print(f"Usado: {used // (2**30)} GB")
+        print(f"Libre: {free // (2**30)} GB")
+        print(f"Porcentaje de uso: {percent_used:.2f}%")
+
+        if percent_used > 80:
+            print("ALERTA: ¡Espacio crítico! Revisa el servidor.")
+        else:
+            print("Estado: Saludable.")
+    except Exception as e:
+        print(f'ERROR: NO SE PUDO LEER EL DISCO. REVISAR {e} ')
 
 # 2. Función para ver quién está conectado (comando 'who' de Linux)
 def check_active_users():
     print("\n--- Usuarios Conectados ---")
     # Ejecutamos un comando de sistema y leemos la salida
     users = os.popen('who').read()
-    if users:
-        print(users)
-    else:
+
+    try:
+
+        if users:
+            print(users)
+    except:
         print("No hay usuarios humanos conectados.")
 
 # 3. Función principal que orquesta todo y guarda un log
